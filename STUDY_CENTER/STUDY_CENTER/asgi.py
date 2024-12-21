6e0baf3b1f1551574df5f13af71d3f3b723b1969
Iskandar <dexqonoviskandar104@gmail.com>
2024-12-21 09:51:12 +0500

    fayllar chiqarildi

---
+++ STUDY_CENTER/STUDY_CENTER/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for STUDY_CENTER project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'STUDY_CENTER.settings')
+
+application = get_asgi_application()
